1a8862ef38757d390b941685deb41a25d4e34ecb
rodritartaglia <rodrigo_tartaglia@hotmail.com>
2023-07-12 23:26:16 -0300

    prueba vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        main: './src/index.html',
-      },
-    },
     outDir: 'dist',
     assetsDir: 'multimedia',
     base: '/funkoshop/', // Ajusta la ruta base según tus necesidades
