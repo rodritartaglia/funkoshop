8ee3fe77b4cf19393331412a5b9d402003d8981d
rodritartaglia <rodrigo_tartaglia@hotmail.com>
2023-07-13 00:10:04 -0300

    prueba multimedia en assets

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsDir: 'multimedia', // Nombre de la carpeta multimedia dentro de tu directorio de salida
-  },
 })
